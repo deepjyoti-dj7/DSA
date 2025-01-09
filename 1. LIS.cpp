1fe0730360225d73240d542e78dde296eacdcf93
Deepjyoti <deepjyoti0072@gmail.com>
2025-01-09 22:36:43 +0530

    Added LIS Memoization

--- 1. LIS.cpp
+++ 1. LIS.cpp
@@ -9,17 +9,35 @@ int LIS(vector<int> &arr, int ind, int prev) {
 
     int take = 0;
     int notTake = LIS(arr, ind + 1, prev);
-    if (prev < arr[ind])
-        take = 1 + LIS(arr, ind + 1, arr[ind]);
+    if (arr[prev] < arr[ind])
+        take = 1 + LIS(arr, ind + 1, ind);
 
     return max(take, notTake);
+}
+
+int LIS_Memoization(vector<int> &arr, int ind, int prev, vector<vector<int>> &dp) {
+    if (ind == arr.size()) 
+        return 0;
+
+    if (dp[ind][prev + 1] != -1) 
+        return dp[ind][prev + 1];
+
+    int take = 0;
+    int notTake = LIS_Memoization(arr, ind + 1, prev, dp);
+    if (arr[prev] < arr[ind])
+        take = 1 + LIS_Memoization(arr, ind + 1, ind, dp);
+
+    return dp[ind][prev + 1] = max(take, notTake);
 }
 
 int main() {
     vector<int> arr = {1, 2, 3, 5, 6, 0, 1};
     int n = arr.size();
 
-    cout << "Length of LIS: " << LIS(arr, 0, INT_MIN) << endl;
+    cout << "Length of LIS: " << LIS(arr, 0, -1) << endl;
+    
+    vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+    cout << "Length of LIS using Memoization: " << LIS_Memoization(arr, 0, -1, dp) << endl;
 
     return 0;
 }
