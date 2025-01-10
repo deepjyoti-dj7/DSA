5f2a2d60ffa1c78a1ac8fef08c2d05591b2c54cd
Deepjyoti <deepjyoti0072@gmail.com>
2025-01-10 20:35:02 +0530

    added LIS java

---
+++ 1.LIS/LIS.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+public class LIS {
+
+    // Recursive approach
+    public static int LIS_Recursive(int[] arr, int ind, int prev) {
+        if (ind == arr.length) 
+            return 0;
+
+        int notTake = LIS_Recursive(arr, ind + 1, prev);
+        int take = 0;
+        if (prev == -1 || arr[prev] < arr[ind]) 
+            take = 1 + LIS_Recursive(arr, ind + 1, ind);
+
+        return Math.max(take, notTake);
+    }
+
+    // Memoization approach
+    public static int LIS_Memoization(int[] arr, int ind, int prev, int[][] dp) {
+        if (ind == arr.length) 
+            return 0;
+
+        if (dp[ind][prev + 1] != -1) 
+            return dp[ind][prev + 1];
+
+        int notTake = LIS_Memoization(arr, ind + 1, prev, dp);
+        int take = 0;
+        if (prev == -1 || arr[prev] < arr[ind]) 
+            take = 1 + LIS_Memoization(arr, ind + 1, ind, dp);
+
+        return dp[ind][prev + 1] = Math.max(take, notTake);
+    }
+
+    // Tabulation approach
+    public static int LIS_Tabulation(int[] arr, int n) {
+        int[][] dp = new int[n + 1][n + 1];
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev = ind - 1; prev >= -1; prev--) {
+                int notTake = dp[ind + 1][prev + 1];
+                int take = 0;
+                if (prev == -1 || arr[prev] < arr[ind]) 
+                    take = 1 + dp[ind + 1][ind + 1];
+
+                dp[ind][prev + 1] = Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
+    }
+
+    // Tabulation with Space Optimization
+    public static int LIS_Tabulation_SpaceOptimized(int[] arr, int n) {
+        int[] cur = new int[n + 1];
+        int[] next = new int[n + 1];
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev = ind - 1; prev >= -1; prev--) {
+                int notTake = next[prev + 1];
+                int take = 0;
+                if (prev == -1 || arr[prev] < arr[ind]) 
+                    take = 1 + next[ind + 1];
+
+                cur[prev + 1] = Math.max(take, notTake);
+            }
+            next = cur.clone();
+        }
+        return next[0];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6, 0, 1};
+        int n = arr.length;
+
+        // Recursive solution
+        System.out.println("Length of LIS (Recursive): " + LIS_Recursive(arr, 0, -1));
+
+        // Memoization solution
+        int[][] dp = new int[n][n + 1];
+        for (int[] row : dp) 
+            Arrays.fill(row, -1);
+        System.out.println("Length of LIS (Memoization): " + LIS_Memoization(arr, 0, -1, dp));
+
+        // Tabulation solution
+        System.out.println("Length of LIS (Tabulation): " + LIS_Tabulation(arr, n));
+
+        // Tabulation with Space Optimization solution
+        System.out.println("Length of LIS (Space Optimized): " + LIS_Tabulation_SpaceOptimized(arr, n));
+    }
+}
