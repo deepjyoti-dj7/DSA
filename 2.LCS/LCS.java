855ea63ce7a53ad679ad00471a6e9b45a69d6fab
Deepjyoti <deepjyoti0072@gmail.com>
2025-01-12 20:25:47 +0530

    Added java and js code

--- 2.LCS/LCS.java
+++ 2.LCS/LCS.java
@@ -64,6 +64,38 @@ public class LCS {
         return prev[m];
     }
 
+    public static String printLCS(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = n, j = m;
+
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1)); 
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1]) 
+                i--; 
+            else
+                j--; 
+        }
+
+        return lcs.reverse().toString(); 
+    }
+
     public static void main(String[] args) {
         String s1 = "abcde";
         String s2 = "ace";
@@ -83,5 +115,9 @@ public class LCS {
 
         // 4. Space Optimized Tabulation
         System.out.println("Length of LCS (Tabulation Space Optimized): " + LCS_TabulationSpaceOptimized(s1, s2));
+
+        // LCS Printing
+        String lcs = printLCS(s1, s2);
+        System.out.println("The LCS is: " + lcs);
     }
 }
--- 2.LCS/LCS.js
+++ 2.LCS/LCS.js
@@ -1,23 +1,34 @@
 // 1. Brute Force Recursive Approach
 function LCS(s1, s2, i, j) {
-  if (i < 0 || j < 0) return 0;
+  if (i < 0 || j < 0) {
+    return 0;
+  }
 
-  if (s1[i] === s2[j]) return 1 + LCS(s1, s2, i - 1, j - 1);
-  else return Math.max(LCS(s1, s2, i - 1, j), LCS(s1, s2, i, j - 1));
+  if (s1[i] === s2[j]) {
+    return 1 + LCS(s1, s2, i - 1, j - 1);
+  } else {
+    return Math.max(LCS(s1, s2, i - 1, j), LCS(s1, s2, i, j - 1));
+  }
 }
 
 // 2. Memoization Approach
 function LCSMemoization(s1, s2, i, j, dp) {
-  if (i < 0 || j < 0) return 0;
+  if (i < 0 || j < 0) {
+    return 0;
+  }
 
-  if (dp[i][j] !== -1) return dp[i][j];
+  if (dp[i][j] !== -1) {
+    return dp[i][j];
+  }
 
-  if (s1[i] === s2[j]) dp[i][j] = 1 + LCSMemoization(s1, s2, i - 1, j - 1, dp);
-  else
+  if (s1[i] === s2[j]) {
+    dp[i][j] = 1 + LCSMemoization(s1, s2, i - 1, j - 1, dp);
+  } else {
     dp[i][j] = Math.max(
       LCSMemoization(s1, s2, i - 1, j, dp),
       LCSMemoization(s1, s2, i, j - 1, dp)
     );
+  }
 
   return dp[i][j];
 }
@@ -61,6 +72,40 @@ function LCSTabulationSpaceOptimized(s1, s2) {
   return prev[m];
 }
 
+function printLCS(s1, s2) {
+  const n = s1.length;
+  const m = s2.length;
+  const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (s1[i - 1] === s2[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  let lcs = [];
+  let i = n;
+  let j = m;
+
+  while (i > 0 && j > 0) {
+    if (s1[i - 1] === s2[j - 1]) {
+      lcs.push(s1[i - 1]);
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return lcs.reverse().join("");
+}
+
 // Main Function
 const s1 = "abcde";
 const s2 = "ace";
@@ -86,3 +131,5 @@ console.log(
   "Length of LCS (Tabulation Space Optimized):",
   LCSTabulationSpaceOptimized(s1, s2)
 );
+
+console.log("The LCS is:", printLCS(s1, s2));
