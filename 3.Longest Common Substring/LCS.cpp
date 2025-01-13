9cdc749e769dc5dc1f5531052d8c12f019de9436
Deepjyoti <deepjyoti0072@gmail.com>
2025-01-13 23:26:13 +0530

    print LCS

--- 3.Longest Common Substring/LCS.cpp
+++ 3.Longest Common Substring/LCS.cpp
@@ -64,12 +64,75 @@ int LCS_Tabulation_SpaceOptimized(const string& s1, const string& s2) {
     return res;
 }
 
+string LCS_Tabulation_Print(const string& s1, const string& s2) {
+    int m = s1.length(), n = s2.length();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    int maxLen = 0, endIndex = -1; 
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                if (dp[i][j] > maxLen) {
+                    maxLen = dp[i][j];
+                    endIndex = i - 1; 
+                }
+            } 
+            else 
+                dp[i][j] = 0; 
+        }
+    }
+
+    string lcs = "";
+    if (maxLen > 0) 
+        lcs = s1.substr(endIndex - maxLen + 1, maxLen);
+
+    cout << "Longest Common Substring: " << lcs << " || and length is: " << maxLen << endl;
+
+    return lcs;
+}
+
+string LCS_Tabulation_SpaceOptimized_Print(const string& s1, const string& s2) {
+    int n = s1.length(), m = s2.length();
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);
+    int maxLen = 0, endIndex = -1; 
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                cur[j] = prev[j - 1] + 1;
+                if (cur[j] > maxLen) {
+                    maxLen = cur[j];
+                    endIndex = i - 1; 
+                }
+            } 
+            else 
+                cur[j] = 0; 
+        }
+        prev = cur;
+    }
+
+    string lcs = "";
+    if (maxLen > 0) 
+        lcs = s1.substr(endIndex - maxLen + 1, maxLen);
+
+    cout << "Longest Common Substring: " << lcs << " || and length is: " << maxLen << endl;
+
+    return lcs;
+}
+
+
 int main() {
     string s1 = "geeksforgeeks";
     string s2 = "practicewritegeekscourses";
+    
     cout << LCS_Iterative(s1, s2) << endl;
     cout << LCS_Recursion(s1, s2) << endl;
     cout << LCS_Tabulation(s1, s2) << endl;
     cout << LCS_Tabulation(s1, s2) << endl;
+
+    LCS_Tabulation_Print(s1, s2);
+    LCS_Tabulation_SpaceOptimized_Print(s1, s2);
+
     return 0;
 }
