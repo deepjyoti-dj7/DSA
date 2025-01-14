c9bf3978f0778e3df26623c121a103481763f22d
Deepjyoti <deepjyoti0072@gmail.com>
2025-01-14 20:42:28 +0530

    Added python code

---
+++ 1.Longest Incresing Subsequence/LCS.py
@@ -0,0 +1,128 @@
+import bisect
+
+def LIS(arr, ind, prev):
+    if ind == len(arr):
+        return 0
+    
+    not_take = LIS(arr, ind + 1, prev)
+    take = 0
+    if prev == -1 or arr[prev] < arr[ind]:
+        take = 1 + LIS(arr, ind + 1, ind)
+    
+    return max(take, not_take)
+
+
+def LIS_memoization(arr, ind, prev, dp):
+    if ind == len(arr):
+        return 0
+    
+    if dp[ind][prev + 1] != -1:
+        return dp[ind][prev + 1]
+    
+    not_take = LIS_memoization(arr, ind + 1, prev, dp)
+    take = 0
+    if prev == -1 or arr[prev] < arr[ind]:
+        take = 1 + LIS_memoization(arr, ind + 1, ind, dp)
+    
+    dp[ind][prev + 1] = max(take, not_take)
+    return dp[ind][prev + 1]
+
+
+def LIS_tabulation(arr, n):
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+    
+    for ind in range(n - 1, -1, -1):
+        for prev in range(ind - 1, -2, -1):
+            take = 0
+            if prev == -1 or arr[prev] < arr[ind]:
+                take = 1 + dp[ind + 1][ind]
+            dp[ind][prev + 1] = max(take, dp[ind + 1][prev + 1])
+    
+    return dp[0][0]
+
+
+def LIS_tabulation_space_optimized_1(arr, n):
+    cur = [0] * (n + 1)
+    next_ = [0] * (n + 1)
+    
+    for ind in range(n - 1, -1, -1):
+        for prev in range(ind - 1, -2, -1):
+            take = 0
+            if prev == -1 or arr[prev] < arr[ind]:
+                take = 1 + next_[ind]
+            cur[prev + 1] = max(take, next_[prev + 1])
+        next_ = cur[:]
+    
+    return next_[0]
+
+
+def LIS_tabulation_space_optimized_2(arr, n):
+    dp = [1] * n
+    maxi = 1
+    
+    for i in range(n):
+        for prev in range(i):
+            if arr[prev] < arr[i]:
+                dp[i] = max(dp[i], 1 + dp[prev])
+        maxi = max(maxi, dp[i])
+    
+    return maxi
+
+
+def LIS_print(arr, n):
+    dp = [1] * n
+    hash_ = list(range(n))
+    maxi = 1
+    last_index = 0
+    
+    for i in range(n):
+        for prev in range(i):
+            if arr[prev] < arr[i] and 1 + dp[prev] > dp[i]:
+                dp[i] = 1 + dp[prev]
+                hash_[i] = prev
+        if dp[i] > maxi:
+            maxi = dp[i]
+            last_index = i
+    
+    temp = []
+    while True:
+        temp.append(arr[last_index])
+        if hash_[last_index] == last_index:
+            break
+        last_index = hash_[last_index]
+    
+    temp.reverse()
+    print("LIS is:", temp)
+    print("Length of LIS is:", maxi)
+    return maxi
+
+
+def LIS_binary_search(arr, n):
+    res = []
+    for num in arr:
+        pos = bisect.bisect_left(res, num)
+        if pos == len(res):
+            res.append(num)
+        else:
+            res[pos] = num
+    return len(res)
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 5, 6, 0, 1]
+    n = len(arr)
+
+    print("Length of LIS using Recursion:", LIS(arr, 0, -1))
+    
+    dp = [[-1] * (n + 1) for _ in range(n)]
+    print("Length of LIS using Memoization:", LIS_memoization(arr, 0, -1, dp))
+
+    print("Length of LIS using Tabulation:", LIS_tabulation(arr, n))
+
+    print("Length of LIS using Tabulation and Space Optimization 1:", LIS_tabulation_space_optimized_1(arr, n))
+
+    print("Length of LIS using Tabulation and Space Optimization 2:", LIS_tabulation_space_optimized_2(arr, n))
+
+    LIS_print(arr, n)
+
+    print("Length of LIS using Binary Search:", LIS_binary_search(arr, n))
---
+++ 2.Longest Common Subsequence/LCS.py
@@ -0,0 +1,101 @@
+def LCS_recursive(s1, s2, i, j):
+    if i < 0 or j < 0:
+        return 0
+    
+    if s1[i] == s2[j]:
+        return 1 + LCS_recursive(s1, s2, i - 1, j - 1)
+    else:
+        return max(LCS_recursive(s1, s2, i - 1, j), LCS_recursive(s1, s2, i, j - 1))
+
+
+def LCS_memoization(s1, s2, i, j, dp):
+    if i < 0 or j < 0:
+        return 0
+    
+    if dp[i][j] != -1:
+        return dp[i][j]
+    
+    if s1[i] == s2[j]:
+        dp[i][j] = 1 + LCS_memoization(s1, s2, i - 1, j - 1, dp)
+    else:
+        dp[i][j] = max(LCS_memoization(s1, s2, i - 1, j, dp), LCS_memoization(s1, s2, i, j - 1, dp))
+    
+    return dp[i][j]
+
+
+def LCS_tabulation(s1, s2):
+    n, m = len(s1), len(s2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    return dp[n][m]
+
+
+def LCS_tabulation_space_optimized(s1, s2):
+    n, m = len(s1), len(s2)
+    prev = [0] * (m + 1)
+    cur = [0] * (m + 1)
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                cur[j] = 1 + prev[j - 1]
+            else:
+                cur[j] = max(prev[j], cur[j - 1])
+        prev = cur[:]
+    
+    return prev[m]
+
+
+def LCS_print(s1, s2):
+    n, m = len(s1), len(s2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    lcs = []
+    i, j = n, m
+    while i > 0 and j > 0:
+        if s1[i - 1] == s2[j - 1]:
+            lcs.append(s1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+    
+    lcs.reverse()
+    return ''.join(lcs)
+
+
+if __name__ == "__main__":
+    s1 = "abcde"
+    s2 = "ace"
+    
+    # Brute Force Recursive
+    print("Length of LCS (Brute Force):", LCS_recursive(s1, s2, len(s1) - 1, len(s2) - 1))
+    
+    # Memoization
+    dp = [[-1 for _ in range(len(s2))] for _ in range(len(s1))]
+    print("Length of LCS (Memoization):", LCS_memoization(s1, s2, len(s1) - 1, len(s2) - 1, dp))
+    
+    # Tabulation
+    print("Length of LCS (Tabulation):", LCS_tabulation(s1, s2))
+    
+    # Space Optimized Tabulation
+    print("Length of LCS (Tabulation Space Optimized):", LCS_tabulation_space_optimized(s1, s2))
+    
+    # LCS Printing
+    print("The LCS is:", LCS_print(s1, s2))
