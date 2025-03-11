cd307c3ee7b15bb0a19d33843a9b368269880a48
Deepjyoti <deepjyoti0072@gmail.com>
2025-03-11 22:12:05 +0530

    Added array problem

---
+++ 01. Arrays/07. Sort 0s 1s 2s/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+    
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low++], nums[mid++]);
+        } else if (nums[mid] == 1) {
+            mid++;
+        } else {
+            swap(nums[mid], nums[high--]);
+        }
+    }
+}
+
+// Main Function
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    sortColors(nums);
+    
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    return 0;
+}
